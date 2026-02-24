d752b79c8bbb07428db50d309809c911d5fe22cd
Alex Winn <alex.winn@du.edu>
2026-02-24 14:35:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const extraText = document.querySelector("#extraText");
 
 // Add event Listener //
 button.addEventListener("click", () => {
+  if (extraText.style.display === "none" || extraText.style.display === "") {
+    // Show text
   extraText.style.display = "block";
-  button.textContent = "Thanks for visiting!";
+  button.textContent = "Read Less";
+  } else {
+    // Hide text
+    extraText.style.display = "none";
+    button.textContent = "Read More";
+  }
 });
