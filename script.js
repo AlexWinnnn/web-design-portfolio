1a846c46e86bea641ec9662df733715efe7ee135
Alex Winn <alex.winn@du.edu>
2026-03-06 15:53:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,3 @@ button.addEventListener("click", () => {
   }
 });
 
-const backToTopBtn = document.getElementById("backToTopBtn");
-
-window.onscroll = function () {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
-    backToTopBtn.style.display = "block";
-} else {
-    backToTopBtn.style.display = "none";
-  }
-};
-
-function scrollToTop () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
-
